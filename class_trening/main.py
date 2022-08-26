33c7ee0517f7e7f0453f417722f5844e0ea763dc
Pawel Latocha <pawel.latocha1990@gmail.com>
2022-08-26 14:08:21 +0200

    Sorting orders

--- class_trening/main.py
+++ class_trening/main.py
@@ -1,17 +1,19 @@
 from shop.apple import Apple
 from shop.order import Order
 from shop.potato import Potato
+import random
+
+def get_order_price(order):
+    return order.total_price
 
 def run_homework():
-    first_order = Order.generate_order(10)
-    print(first_order)
-    second_order = Order.generate_order(5)
-    print(second_order)
-    # old_potato = Potato(species="Old Potato", size="S", price=0.2)
-    # print(old_potato)
-    # red_apple = Apple(species="Red", size="M", price=2.5)
-    # print(red_apple)
-    # print(len(first_order))
+    orders = []
+    for _ in range(5):
+        orders.append(Order.generate_order(random.randint(1,30)))
+
+    orders.sort(key=get_order_price)
+    for order in orders:
+        print(order)
 
 if __name__ == '__main__':
     run_homework()
