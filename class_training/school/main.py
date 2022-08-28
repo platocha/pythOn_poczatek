68bd554540b9f72b936799f73c43f5ddc5fc0c25
Pawel Latocha <pawel.latocha1990@gmail.com>
2022-08-28 14:16:49 +0200

    Add school project

---
+++ class_training/school/main.py
@@ -0,0 +1,10 @@
+from estudent import data_generator
+from school import School
+
+def run_example():
+    students = data_generator.generate_students()
+    school = School(name="Hogwart", students=students)
+    print(school)
+
+if __name__ == '__main__':
+    run_example()
