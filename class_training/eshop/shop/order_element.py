348c0c620cf885cc70f03f593185cab3c0475292
Pawel Latocha <pawel.latocha1990@gmail.com>
2022-09-05 16:04:42 +0200

    Change Product, ProductExpiration and OrderElement to dataclasses

--- class_training/eshop/shop/order_element.py
+++ class_training/eshop/shop/order_element.py
@@ -1,17 +1,12 @@
+from dataclasses import dataclass
+
+
 class OrderElement:
-    def __init__(self, product, quantity):
-        self.product = product
-        self.quantity = quantity
+    product: str
+    quantity: int
 
     def calculate_price(self):
         return self.product.price * self.quantity
 
     def __str__(self):
         return f"{self.product}\t\t x {self.quantity}\n"
-
-    def __eq__(self, other):
-        if self.__class__ != other.__class__:
-            return NotImplemented
-        else:
-            return (self.product == other.product and
-                    self.quantity == other.quantity)
--- class_training/eshop/shop/product.py
+++ class_training/eshop/shop/product.py
@@ -1,27 +1,19 @@
+from dataclasses import dataclass
+
+@dataclass
 class Product:
-    def __init__(self, name, category, price, identifier):
-        self.name = name
-        self.category = category
-        self.price = price
-        self.identifier = identifier
+    name: str
+    category: str
+    price: float
+    identifier: int
 
     def __str__(self):
         return(f"ID: {self.identifier} | Nazwa: {self.name} | Kategoria: {self.category} | Cena: {self.price}/szt")
 
-    def __eq__(self, other):
-        if self.__class__ != other.__class__:
-            return NotImplemented
-        else:
-            return (self.name == other.name and
-                    self.category == other.category and
-                    self.price == other.price)
-
+@dataclass
 class ProductExpiration(Product):
-
-    def __init__(self, name, category, price, identifier, production_year, expiration_years):
-        super().__init__(name, category, price, identifier)
-        self.production_year = production_year
-        self.expiration_years = expiration_years
+    production_year: int
+    expiration_years: int
 
     def does_expire(self, current_year):
         return current_year > self.production_year + self.expiration_years
