428c36d71a65c84a0374cea7691f2e8105954356
Pawel Latocha <pawel.latocha1990@gmail.com>
2022-09-10 10:52:37 +0200

    Save students to txt file

--- class_training/school/main.py
+++ class_training/school/main.py
@@ -1,9 +1,20 @@
 from estudent.data_generator import generate_students
-from estudent.school import School
+import os
 def run_example():
-    students = generate_students(number_of_students=250)
-    school = School(name="Mala szkola", students=[])
-    school.students = students
+    students = generate_students()
+
+    students_data_file_path = os.path.join("data", "students.txt")
+    with open(students_data_file_path, mode="w") as students_file:
+        for student in students:
+            students_file.write(str(student))
+            students_file.write("\n")
+
+    new_students = generate_students()
+    with open(students_data_file_path, mode="a") as students_file:
+        students_file.write("Dodatkowo dopisalismy jeszcze:\n")
+        for student in new_students:
+            students_file.write(str(student))
+            students_file.write("\n")
 
 
 if __name__ == '__main__':
